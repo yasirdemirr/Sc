480d5ff4dfea9468739ecb9ff60e0958065f2c12
Yasir Demir <yasir.demirr@gmail.com>
2021-08-30 20:40:23 +0300

    [DEV][yasir.demir][timermanager and use case implementation]

--- app/src/main/java/com/myd/sccasestudy/home/ui/MainViewModel.kt
+++ app/src/main/java/com/myd/sccasestudy/home/ui/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.myd.sccasestudy.home.ui
 
+import android.view.View
+import androidx.databinding.ObservableInt
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.myd.sccasestudy.core.base.baseinterface.CountDownTimerListener
+import com.myd.sccasestudy.core.base.baseinterface.ITimerManager
 import com.myd.sccasestudy.core.model.Result
+import com.myd.sccasestudy.home.data.model.PeopleState
 import com.myd.sccasestudy.home.interactor.GetPeopleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,22 +17,58 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val peopleUseCase: GetPeopleUseCase
-) : ViewModel() {
-    fun getPeople() {
+    private val peopleUseCase: GetPeopleUseCase,
+    private val timerManager: ITimerManager
+) : ViewModel(), CountDownTimerListener {
+
+    var paginationValue: String? = null
+    var isLoading = true
+    var isLoadingObservable = ObservableInt(View.GONE)
+
+    var requestAvailable = true
+
+    private val peopleMutableLiveData = MutableLiveData<PeopleState>()
+    val peopleLiveData: LiveData<PeopleState> = peopleMutableLiveData
+
+
+    fun getPeople(nextValue: String?) {
+        isLoadingObservable.set(View.VISIBLE)
         viewModelScope.launch {
             when (val result = peopleUseCase.execute(
-                GetPeopleUseCase.Params("yas")
+                GetPeopleUseCase.Params(nextValue)
             )) {
                 is Result.Success -> {
-                    val x = result.payload
-                    val y = x
+                    if (result.payload?.fetchResponse?.people.isNullOrEmpty()) {
+                        peopleMutableLiveData.value = PeopleState(
+                            fetchError = "No One Is Here"
+                        )
+                        isLoadingObservable.set(View.GONE)
+                        return@launch
+                    }
+                    paginationValue = result.payload?.fetchResponse?.next
+                    peopleMutableLiveData.value = PeopleState(
+                        fetchResponse = result.payload?.fetchResponse
+                    )
+                    isLoadingObservable.set(View.GONE)
                 }
                 is Result.Error -> {
-                    val x = result.error
-                    val y = x
+                    peopleMutableLiveData.value = PeopleState(
+                        fetchError = result.error
+                    )
+                    isLoadingObservable.set(View.GONE)
                 }
+
             }
         }
+    }
+
+    fun initTimer() {
+        timerManager.addTimer(this)
+        timerManager.starTimer()
+        requestAvailable = false
+    }
+
+    override fun onFinish() {
+        requestAvailable = true
     }
 }
